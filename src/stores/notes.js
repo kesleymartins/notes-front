d8c23aa07705c813dc496428a56adacf9379a803
kesleymartins <kesleymartins>
2023-04-07 13:15:07 -0300

    connect application with the api

--- src/stores/notes.js
+++ src/stores/notes.js
@@ -1,16 +1,26 @@
 import { defineStore } from "pinia"
+import api from "../services/api"
 
 export const useNotesStore = defineStore('notes', {
   state: () => ({
-    notes: []
+    notes: [],
   }),
   actions: {
+    fetchNotes() {
+      api.get('/notes')
+      .then(res => {
+        this.notes = res.data
+      })
+    },
     addNote(newNote) {
-      this.notes.push({
-        id: this.notes.length + 1,
-        text: newNote.value,
-        date: new Date(),
-        backgroundColor: `hsl(${Math.random() * 360}, 100%, 75%)`
+      const note = {
+        content: newNote.value,
+        background_color: `hsl(${Math.random() * 360}, 100%, 75%)`
+      }
+
+      api.post('/notes', note)
+      .then(res => {
+        this.notes.push(res.data)
       })
     }
   }
