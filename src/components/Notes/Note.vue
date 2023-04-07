59c7376702906e2937753bad577cee8e1b8a21f4
kesleymartins <kesleymartins>
2023-04-07 13:52:37 -0300

    add remove icon to note

--- src/components/Notes/Note.vue
+++ src/components/Notes/Note.vue
@@ -1,15 +1,36 @@
 <script setup>
+import { computed } from '@vue/reactivity';
 import Card from '../Card.vue'
 
 const props = defineProps(['note'])
+
+const noteDate = computed(() => {
+  const date = new Date(props.note.created_at)
+  return `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`
+})
 </script>
 
 <template>
   <Card :style="{backgroundColor: note.background_color}">
     <span>{{ note.content }}</span>
-    <span>{{ (new Date(note.created_at)).toLocaleString('pt-BR') }}</span>
+
+    <div class="bottom">
+      <span>{{ noteDate }}</span>
+
+      <span class="icon">
+        <v-icon name="md-delete" fill="#800080" :hover="true" animation="pulse"/>
+      </span>
+    </div>
   </Card>
 </template>
 
 <style scoped>
+.bottom {
+  display: flex;
+  justify-content: space-between;
+}
+
+.bottom .icon {
+  cursor: pointer;
+}
 </style>
