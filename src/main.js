65409c0d8c9428194dde354d886af66185dcb5e3
kesleymartins <kesleymartins>
2023-04-07 13:52:03 -0300

    setup vue icons

--- src/main.js
+++ src/main.js
@@ -4,8 +4,14 @@ import App from './App.vue'
 
 import './assets/reset.css'
 
+import { OhVueIcon, addIcons } from "oh-vue-icons"
+import { MdDelete } from "oh-vue-icons/icons";
+
+addIcons(MdDelete)
+
 const pinia = createPinia()
 const app = createApp(App)
 
 app.use(pinia)
+app.component("v-icon", OhVueIcon)
 app.mount('#app')
